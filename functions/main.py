b923bd10bceef992d9ccfe8bbf3fcbd09c04c1bd
Aravind Ramachandran <aravind.ramachandran@gmail.com>
2023-10-05 21:36:03 +0530

    Functions work too with imports

--- functions/main.py
+++ functions/main.py
@@ -5,9 +5,18 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
+import testfns
+# import googleops
+
 initialize_app()
 #
 #
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+    retval = testfns.hello()
+    return https_fn.Response(retval)
+
+@https_fn.on_request()
+def on_get_creds(req: https_fn.Request) -> https_fn.Response:
+    # retval = googleops.get_creds()
+    return https_fn.Response('That worked??')
